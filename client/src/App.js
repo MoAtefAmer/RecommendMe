c105b6bec33f3f9487d7412005f3734a6ceb0ec3
MohamedAtef <mo.atefamer@gmail.com>
2020-02-25 21:19:12 +0200

    Student Signup complete; navbar placed

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,28 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import SignUp from "./components/pages/SignUp"
+import SignUp from "./components/pages/SignUp";
+import ButtonAppBar from "./components/Surfaces/ButtonAppBar"
 
+
+export var TestContext=React.createContext() 
 export class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-  
-    };
-  
+    this.state = {};
   }
+  
   render() {
+    
     return (
       <div>
+<ButtonAppBar/>
         <Router>
           <React.Fragment>
-            <Route exact path="/studentSignup" render={() => 
-            <SignUp/>
-            
-            
-            } />
+            <Route exact path="/studentSignup" render={() => <TestContext.Provider value={{
+              "foo":"test"
+            }}><SignUp  /></TestContext.Provider>} />
+          
           </React.Fragment>
         </Router>
       </div>
--- client/src/components/pages/SignUp.js
+++ client/src/components/pages/SignUp.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -8,13 +8,15 @@ import clsx from "clsx";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import LockOutlinedIcon from "@material-ui/icons/AccountBox";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Switch from "@material-ui/core/Switch";
 import { grey, lightBlue, green } from "@material-ui/core/colors";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import {TestContext} from "../../App"
+
 
 function Copyright() {
   return (
@@ -44,7 +46,10 @@ const useStyles = makeStyles(theme => ({
   submit: {
     margin: theme.spacing(3, 0, 2)
   },
-  
+  paperContainer: {
+    backgroundImage: `url(${Image})`
+},
+
   buttonProgress: {
     color: green[500],
     position: "absolute",
@@ -59,7 +64,7 @@ const useStyles = makeStyles(theme => ({
       backgroundColor: green[700]
     }
   },
-  normalForm:{
+  normalForm: {
     backgroundColor: lightBlue[900],
     "&:hover": {
       backgroundColor: grey[900]
@@ -71,23 +76,15 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-// const ColorButton = withStyles(theme => ({
-//   root: {
-//     color: theme.palette.getContrastText(lightBlue[900]),
-  
-//   },
-    
-//   }
-// }))(Button);
 
 const ViewSwitch = withStyles({
   switchBase: {
     color: grey[300],
     "&$checked": {
-      color: grey[900]
+      color: green[700]
     },
     "&$checked + $track": {
-      backgroundColor: grey[900]
+      backgroundColor: green[900]
     }
   },
   checked: {},
@@ -100,23 +97,118 @@ export default function SignUp() {
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [viewRecommendation, setViewRecommendation] = useState(false);
   const [loading, setLoading] = useState(false);
   const [success, setSuccess] = useState(false);
+
+  //Validations
+  const [firstNameError, setFirstNameError] = useState("");
+  const [firstNameErrorToggle, setFirstNameErrorToggle] = useState(false);
+  const [lastNameError, setLastNameError] = useState("");
+  const [lastNameErrorToggle, setLastNameErrorToggle] = useState(false);
+  const [emailError, setEmailError] = useState("");
+  const [emailErrorToggle, setEmailErrorToggle] = useState(false);
+  const [passwordError, setPasswordError] = useState("");
+  const [passwordErrorToggle, setPasswordErrorToggle] = useState(false);
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
+  const [confirmPasswordErrorToggle, setConfirmPasswordErrorToggle] = useState(false);
+
+//Test context
+const sss= useContext(TestContext)
+console.log(sss)
 
   const buttonClassname = clsx({
     [classes.buttonSuccess]: success,
     [classes.normalForm]: !success
   });
 
+  const validate = () => {
+    let isError = false;
+    const errors = {};
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (firstName.length <=0 ) {
+      isError = true;
+      errors.firstNameError = "Please fill in the field";
+      errors.firstNameErrorToggle=true;
+    }else{
+      setFirstNameErrorToggle(false)
+      setFirstNameError("")
+    }
+
+    if (lastName.length <=0 ) {
+      isError = true;
+      errors.lastNameError = "Please fill in the field";
+      errors.lastNameErrorToggle=true;
+    }else{
+      setLastNameErrorToggle(false)
+      setLastNameError("")
+    }
+
+    if(!(re.test(email))){
+      isError=true;
+      errors.emailError="Please enter a valid email"
+      errors.emailErrorToggle=true
+    }else{
+      setEmailErrorToggle(false)
+      setEmailError("")
+    }
+
+    if (password.length <8 ) {
+      isError = true;
+      errors.passwordError = "Password must be 8 characters or more";
+      errors.passwordErrorToggle=true;
+    }else{
+      setPasswordErrorToggle(false)
+      setPasswordError("")
+    }
+
+    if (confirmPassword.length <8 ) {
+      isError = true;
+      errors.confirmPasswordError = "Password must be 8 characters or more";
+      errors.confirmPasswordErrorToggle=true;
+    }else{
+      setConfirmPasswordErrorToggle(false)
+      setConfirmPasswordError("")
+    }
+    
+    if(!(password===confirmPassword)){
+      isError=true;
+      errors.passwordError = "Passwords do not match ";
+      errors.passwordErrorToggle=true;
+      errors.confirmPasswordError = "Passwords do not match";
+      errors.confirmPasswordErrorToggle=true;
+    }
+
+
+    if (isError) {
+      setFirstNameError(errors.firstNameError);
+      setFirstNameErrorToggle(errors.firstNameErrorToggle)
+      setLastNameError(errors.lastNameError);
+      setLastNameErrorToggle(errors.lastNameErrorToggle)
+      setEmailError(errors.emailError)
+      setEmailErrorToggle(errors.emailErrorToggle)
+      setPasswordError(errors.passwordError)
+      setPasswordErrorToggle(errors.passwordErrorToggle)
+      setConfirmPasswordError(errors.confirmPasswordError)
+      setConfirmPasswordErrorToggle(errors.confirmPasswordErrorToggle)
+    }
+
+    return isError;
+  };
+
   const handleSignUp = e => {
     e.preventDefault();
     console.log("button clicked");
 
-    if (!loading) {
-      setLoading(true);
-    
-    }
+   
+    const err = validate();
+
+    if(!err){
+      if (!loading) {
+        setLoading(true);
+      }
     fetch(`http://localhost:3000/api/student/studentSignup`, {
       method: "POST",
       body: JSON.stringify({
@@ -128,40 +220,39 @@ export default function SignUp() {
       headers: {
         "Content-Type": "application/json"
       }
-    }).then((res) => {
+    }).then(res => {
       console.log(res.status);
-if(res.status===200){
-  setLoading(false);
-  setSuccess(true);
-  setEmail("");
-      setPassword("");
-      setFirstName("");
-      setLastName("");
-      setTimeout(()=>(document.location.href = "/login"), 4000);
-}else{
-  setLoading(false);
-}
-
-      
+      if (res.status === 200) {
+        setLoading(false);
+        setSuccess(true);
+        setEmail("");
+        setPassword("");
+        setFirstName("");
+        setLastName("");
+        setTimeout(() => (document.location.href = "/login"), 4000);
+      } else {
+        setLoading(false);
+      }
     });
   };
-
+  }
   // console.log(firstName)
   // console.log(lastName)
   // console.log(email)
   // console.log(password)
   //console.log(viewRecommendation);
-  console.log("loading:" + loading);
-  console.log("success:" + success);
+  // console.log("loading:" + loading);
+  // console.log("success:" + success);
+ 
   return (
-    <Container component="main" maxWidth="xs">
+    <Container  component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Student Sign up
+          Student SignUp
         </Typography>
         <form className={classes.form} noValidate>
           <Grid container spacing={2}>
@@ -171,6 +262,8 @@ if(res.status===200){
                 name="firstName"
                 variant="outlined"
                 required
+                helperText={firstNameError}
+                error={firstNameErrorToggle}
                 fullWidth
                 id="firstName"
                 label="First Name"
@@ -184,9 +277,10 @@ if(res.status===200){
             <Grid item xs={12} sm={6}>
               <TextField
                 variant="outlined"
-                required
                 fullWidth
                 id="lastName"
+                helperText={lastNameError}
+                error={lastNameErrorToggle}
                 label="Last Name"
                 name="lastName"
                 autoComplete="lname"
@@ -199,8 +293,11 @@ if(res.status===200){
             <Grid item xs={12}>
               <TextField
                 variant="outlined"
+                type="email"
                 required
                 fullWidth
+                helperText={emailError}
+                error={emailErrorToggle}
                 id="email"
                 label="Email Address"
                 name="email"
@@ -220,10 +317,30 @@ if(res.status===200){
                 label="Password"
                 type="password"
                 id="password"
+                helperText={passwordError}
+                error={passwordErrorToggle}
                 autoComplete="current-password"
                 value={password}
                 onChange={e => {
                   setPassword(e.target.value);
+                }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                helperText={confirmPasswordError}
+                error={confirmPasswordErrorToggle}
+                name="confirm password"
+                label="Confirm Password"
+                type="password"
+                id="confirm password"
+                autoComplete="current-password"
+                value={confirmPassword}
+                onChange={e => {
+                  setConfirmPassword(e.target.value);
                 }}
               />
             </Grid>
@@ -258,10 +375,9 @@ if(res.status===200){
               <CircularProgress size={24} className={classes.buttonProgress} />
             )}
           </div>
-
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link href="login" variant="body2">
                 Already have an account? Sign in
               </Link>
             </Grid>
@@ -271,6 +387,7 @@ if(res.status===200){
       <Box mt={5}>
         <Copyright />
       </Box>
+     
     </Container>
   );
 }
