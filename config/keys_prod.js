0a566b7c88fd03d9b8566e3514134732db001dac
MohamedAtef <mo.atefamer@gmail.com>
2020-04-01 20:55:18 +0200

    completed environment variables

---
+++ config/keys_prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  EMAIL:process.env.EMAIL,  
+  PASSWORD:process.env.PASSWORD,
+  PUSHER_APP_ID:process.env.PUSHER_APP_ID,
+  PUSHER_APP_KEY:process.env.PUSHER_APP_KEY,
+  PUSHER_APP_SECRET:process.env.PUSHER_APP_SECRET
+}
